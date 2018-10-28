95f9448949c1c76d75470b284eac25a47620f87b
Pavel <yourproekt@gmail.com>
2018-10-28 20:10:04 +0200

    Update reverseArrayInPlace.js

--- array/reverseArrayInPlace.js
+++ array/reverseArrayInPlace.js
@@ -1,15 +1,15 @@
 /*Нужно сделать реверс массива без метода reverse с мутацией входящего массива (edited)*/
+
+
 function reverseArrayInPlace(arr){
-
-for(var i = 0; i < arr.length; i++ ){
-delete arr[i];
-arr[i] = arr.length-i;
+  var lengthAr = arr.length;
+  for(var i = arr.length-1; i >= 0; i-- ){
+    arr.push(arr[i]);
+  }
+  arr.splice(0,lengthAr);
 }
 
-
-}
-
-var arrayValue = [1, 2, 3, 4, 5];
+var arrayValue = [1, 2, 3, 4, 5];;
 console.log(arrayValue);
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
