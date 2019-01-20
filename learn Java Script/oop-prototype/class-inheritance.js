e47868377da9587998ea63a36a5a27fa68a99771
Pavel <yourproekt@gmail.com>
2019-01-20 22:14:48 +0200

    Update class-inheritance.js

--- learn Java Script/oop-prototype/class-inheritance.js
+++ learn Java Script/oop-prototype/class-inheritance.js
@@ -1,4 +1,99 @@
 /*
+№1
+Перепишите в виде класса
+
+Есть класс CoffeeMachine, заданный в функциональном стиле.
+Задача: переписать CoffeeMachine в виде класса с использованием прототипа.
+Исходный код:
+
+ function CoffeeMachine(power) {
+  var waterAmount = 0;
+
+  var WATER_HEAT_CAPACITY = 4200;
+
+  function getTimeToBoil() {
+    return waterAmount * WATER_HEAT_CAPACITY * 80 / power;
+  }
+
+  this.run = function() {
+    setTimeout(function() {
+      alert( 'Кофе готов!' );
+    }, getTimeToBoil());
+  };
+
+  this.setWaterAmount = function(amount) {
+    waterAmount = amount;
+  };
+
+}
+
+var coffeeMachine = new CoffeeMachine(10000);
+coffeeMachine.setWaterAmount(50);
+coffeeMachine.run();
+P.S. При описании через прототипы локальные переменные недоступны методам, поэтому нужно будет переделать их в защищённые свойства.
+
+*/
+function CoffeeMachine(power) {
+    this._power = power;
+    this._waterAmount = 0;
+}
+
+CoffeeMachine.prototype._WATER_HEAT_CAPACITY = 4200;
+
+CoffeeMachine.prototype.getTimeToBoil = function() {
+    return this._waterAmount * this._WATER_HEAT_CAPACITY * 80 / this._power;
+}
+
+CoffeeMachine.prototype.run = function() {
+    var self = this;
+    setTimeout(function() {
+        alert(`Кофе готов! ${self._waterAmount}`);
+    }, this.getTimeToBoil());
+};
+
+CoffeeMachine.prototype.setWaterAmount = function(amount) {
+    this._waterAmount = amount;
+};
+
+var coffeeMachine = new CoffeeMachine(10000);
+coffeeMachine.setWaterAmount(50);
+coffeeMachine.run();
+
+/*-------------------------- ES6 ---------------------------*/
+
+'use strict';
+class CoffeeMachine {
+    constructor(power) {
+        this._power = power;
+        this._waterAmount = 0;
+        this._WATER_HEAT_CAPACITY = 4200;
+    }
+
+    getTimeToBoil() {
+        return this._waterAmount * this._WATER_HEAT_CAPACITY * 80 / this._power;
+    }
+
+    run() {
+        var self = this;
+        setTimeout(function() {
+            alert(`Кофе готов! ${self._waterAmount}`);
+        }, this.getTimeToBoil());
+    };
+
+    set WaterAmount(amount) {
+        this._waterAmount = amount;
+    };
+}
+
+var coffeeMachine = new CoffeeMachine(10000);
+coffeeMachine.WaterAmount = 50;
+coffeeMachine.run();
+
+/*-------------------------- ES6 ---------------------------*/
+
+/*--------------------------------------------*/
+
+/*
 №3
 Класс "часы"
 
@@ -7,6 +102,7 @@
 Задача: переписать часы на прототипах. Приватные свойства и методы сделать защищёнными.
 P.S. Часики тикают в браузерной консоли (надо открыть её, чтобы увидеть).
 */
+
 function Clock(options) {
     this._ClockOption = options;
     this._timer = 0;
@@ -46,12 +142,61 @@ var clock = new Clock({
 });
 clock.start();
 
+
+
+/*-------------------------- ES6 ---------------------------*/
+
+
+class Clock {
+    constructor(options) {
+        this._ClockOptionTmpl = options.template;
+        this._timer = 0;
+    }
+
+    render() {
+        var date = new Date();
+
+        var hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+
+        var min = date.getMinutes();
+        if (min < 10) min = '0' + min;
+
+        var sec = date.getSeconds();
+        if (sec < 10) sec = '0' + sec;
+
+        var output = this._ClockOptionTmpl.replace('h', hours).replace('m', min).replace('s', sec);
+
+        console.log(output);
+    }
+
+    stop() {
+        clearInterval(this._timer);
+    }
+
+    start() {
+        this._render();
+        this._timer = setInterval(() => {
+            this.render()
+        }, 1000);
+    }
+};
+
+var clock = new Clock({
+    template: 'h:m:s'
+});
+
+clock.start();
+
+
+/*-------------------------- ES6 ---------------------------*/
+
 /*--------------------------------------------*/
 
 /*
 №4
 Класс "расширенные часы"
-важность: 5
+
 Есть реализация часиков на прототипах. Создайте класс, расширяющий её,
 добавляющий поддержку параметра precision, который будет задавать частоту тика в setInterval.
 Значение по умолчанию: 1000.
@@ -131,6 +276,72 @@ var lowResolutionClock = new ExtendedClock({
 });
 
 lowResolutionClock.start();
+
+
+/*-------------------------- ES6 ---------------------------*/
+
+
+class Clock {
+    constructor(options) {
+        this._template = options.template;
+    }
+
+
+    _render() {
+        var date = new Date();
+
+        var hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+
+        var min = date.getMinutes();
+        if (min < 10) min = '0' + min;
+
+        var sec = date.getSeconds();
+        if (sec < 10) sec = '0' + sec;
+
+        var output = this._template.replace('h', hours).replace('m', min).replace('s', sec);
+
+        console.log(output);
+    };
+
+    stop() {
+        clearInterval(this._timer);
+    };
+
+    start() {
+        this._render();
+        this._timer = setInterval(() => {
+            this._render();
+        }, 1000);
+    };
+}
+
+class ExtendedClock extends Clock {
+    constructor(option) {
+        super(...arguments);
+        this._ClockPrecision = +option.precision || 1000;
+    }
+
+    start() {
+        this._render();
+        this._timer = setInterval(() => {
+            this._render();
+        }, this._ClockPrecision);
+    };
+
+}
+
+var lowResolutionClock = new ExtendedClock({
+    template: 'h:m:s',
+    precision: 2000
+});
+
+lowResolutionClock.start();
+
+
+/*-------------------------- ES6 ---------------------------*/
+
+
 /*--------------------------------------------*/
 
 /*
@@ -175,7 +386,7 @@ Menu.prototype._stateAsString = function() {
 Menu.prototype.showState = function() {
     alert(this._stateAsString());
 };
-/*--------------------------------------*/
+/*----*/
 function AnimatingMenu() {
     Menu.apply(this, arguments);
 };
@@ -220,3 +431,88 @@ setTimeout(function() { // через 1 секунду
     menu.close();
     menu.showState(); // закрыто
 }, 1200);
+
+/*-------------------------- ES6 ---------------------------*/
+
+
+class Menu {
+    constructor(state) {
+        this.STATE_OPEN = 1;
+        this.STATE_CLOSED = 0;
+        this._state = state || this.STATE_CLOSED;
+    }
+
+    open() {
+        this._state = this.STATE_OPEN;
+    };
+
+    close() {
+        this._state = this.STATE_CLOSED;
+    };
+
+    _stateAsString() {
+
+        switch (this._state) {
+            case this.STATE_OPEN:
+                return 'открыто';
+
+            case this.STATE_CLOSED:
+                return 'закрыто';
+        }
+    };
+
+    showState() {
+        alert(this._stateAsString());
+    };
+
+
+};
+/*----*/
+
+class AnimatingMenu extends Menu {
+    constructor() {
+        super(0);
+        this.STATE_ANIMATING = 2;
+    }
+
+    open() {
+        var _this = this;
+        this._state = this.STATE_ANIMATING;
+        this._timer = setTimeout(() => {
+            super.open();
+        }, 1000);
+    };
+
+    close() {
+        clearTimeout(this._timer);
+        super.close();
+    };
+
+    _stateAsString() {
+        switch (this._state) {
+            case this.STATE_ANIMATING:
+                return 'анимация';
+            default:
+                return super._stateAsString();
+        }
+    };
+
+};
+
+
+var menu = new AnimatingMenu();
+
+menu.showState(); // закрыто
+
+menu.open();
+menu.showState(); // анимация
+
+setTimeout(function() { // через 1 секунду
+    menu.showState(); // открыто
+
+    menu.close();
+    menu.showState(); // закрыто
+}, 1200);
+
+
+/*-------------------------- ES6 ---------------------------*/
